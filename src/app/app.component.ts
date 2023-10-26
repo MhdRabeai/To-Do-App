7ef56af6bfd3ba9b6e163ee1d2a05b1e07648bac
MhdRabeai <mhd.rabea.naser@gmail.com>
2023-10-26 22:34:14 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'to-do-app';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+
+      if (this.todos.length <= 7) {
+        this.todos.push(todo);
+        this.newTodo = '';
+      } else {
+        alert('Array Of Todos is Full');
+        this.newTodo = '';
+      }
+    } else {
+      alert('Please , Enter a Todo !!');
+    }
+  }
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+  remove(id: number) {
+    this.todos = this.todos.filter((e, i) => i !== id);
+  }
 }
